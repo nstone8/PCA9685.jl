6c05a7e06c1851b6246ec623041d50553a06a2d8
Nick Stone <nicholasestone@proton.me>
2025-09-24 15:05:24 -0500

    Implemented initial functionality

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,3 @@
 using Conda
 Conda.pip_interop(true)
-Conda.pip("install",["adafruit-circuitpython-pca9685",
-                     "adafruit-circuitpython-servokit"])
+Conda.pip("install",["adafruit-circuitpython-servokit"])
--- src/PCA9685.jl
+++ src/PCA9685.jl
@@ -1,5 +1,35 @@
 module PCA9685
 
-greet() = print("Hello World!")
+using PyCall
+
+export ServoDriver, setthrottle!
+
+"""
+```julia
+ServoDriver(i2c)
+```
+Create a ServoDriver object controlling a servo driver connected to the `i2c` bus
+"""
+struct ServoDriver
+    kit #python library
+    function ServoDriver(i2c)
+        servokit = pyimport("adafruit_servokit").ServoKit
+        kit = servokit(channels=16,i2c=i2c)
+        new(kit)
+    end
+end
+
+"""
+```julia
+setthrottle!(s,i,throttle)
+```
+Set the throttle on a continuous servo connected to channel `i` on a `ServoDriver`
+"""
+function setthrottle!(s::ServoDriver,i::Int,throttle::Real)
+    #pycall will attempt to shift from 1 indexed arrays to 0 indexed, but all the
+    #boards are labeled with 0 indexed addresses, so we need to throw an i+1 in here
+    s.kit.continuous_servo[i+1].throttle = throttle
+    return nothing
+end
 
 end # module PCA9685
